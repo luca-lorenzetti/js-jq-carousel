4164bfccc7c85c38e3236af5a39d916eb46e99b2
luca <luca.lorenzetti0@gmail.com>
2021-03-12 15:52:35 +0100

    Modifica active bullet

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(function(){
 
     var nextButton = $('div.next');
     var prevButton = $('div.prev');
-
+    
 
 
     nextButton.click({mode: "next"}, changeImage);
@@ -10,25 +10,40 @@ $(function(){
 
     function changeImage(event){
         var activeImage = $('.images img.active');
+        var bulletActive = $('i.active');
 
         if( event.data.mode == "next" ){
             if( $(activeImage).hasClass("last")  ){
+                 //immagine
                 $(activeImage).toggleClass("active");
                 $(".images img.first").toggleClass("active");
+                //bullet
+                $(bulletActive).toggleClass("active");
+                $("i.first").toggleClass("active");
             }
             else{
+                //immagine
                 $(activeImage).toggleClass("active").next().toggleClass("active");
+                //bullet
+                $(bulletActive).toggleClass("active").next().toggleClass("active")
             }
-
         }
-        if( event.data.mode == "prev" ){
-            if( $(activeImage).hasClass("first")  ){
-                $(activeImage).toggleClass("active");
-                $(".images img.last").toggleClass("active");
-            }
-            else{
-                $(activeImage).toggleClass("active").prev().toggleClass("active");
-            }
+
+        else if( event.data.mode == "prev" ){
+                if( $(activeImage).hasClass("first")  ){
+                //immagine
+                    $(activeImage).toggleClass("active");
+                    $(".images img.last").toggleClass("active");
+                    //bullet
+                    $(bulletActive).toggleClass("active");
+                    $("i.last").toggleClass("active");
+                }
+                else{
+                    //immagine
+                    $(activeImage).toggleClass("active").prev().toggleClass("active");
+                    //bullet
+                    $(bulletActive).toggleClass("active").prev().toggleClass("active")
+                }
 
         }
     
